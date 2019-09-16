96753ac3f03fda2783df38efb7404bc4faa3f9e9
gilsu <rlftn510@naver.com>
2019-09-16 17:36:32 +0900

    webpack 기본설정 npm init npm vue install npm install webpack webpack-cli -D

---
+++ baseball/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry : {
+        app : './main.js'
+    },
+    module : {
+        rules: [{
+
+        }]
+    },
+    plugin : [],
+    output : {
+        filename : '[name].js',
+        path : './dist'
+    }
+}
