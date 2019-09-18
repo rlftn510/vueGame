1151a68390d4ca65ec4e58db7bfcc63e860372e6
gilsu <rlftn510@naver.com>
2019-09-18 19:50:38 +0900

    반응속도 게임

--- speed/responseCheck.vue
+++ speed/responseCheck.vue
@@ -2,32 +2,47 @@
   <div>
     <div id="screen" v-bind:class="state" @click="onClickScreen">{{message}}</div>
     <div>
-      <p>평균asd시간 {{state}}</p>
-      <button @submit="onReset">리셋</button>
+      <p>평균시간 {{result.reduce((a, c) => a + c, 0) / result.length || 0 }}ms</p>
+      <button @click="onReset">리셋</button>
     </div>
   </div>
 </template>
 
 <script>
+  let startTime = 0
+  let endTime = 0
+  let timeout = null 
+  // 화면이랑 관련이 있는 것만 data, 그렇지 않으면 변수
   export default {
       data(){
         return {
           result : [],
           state: 'waiting',
-          message : 'asd'
+          message : '클릭해소 시작하세요'
         }
       },
       methods : {
         onReset() {
-
+          this.result = []
         },
         onClickScreen (){ 
           if(this.state === 'waiting') {
             this.state = 'ready'
+            this.message = '초록색이 되면 클릭하세요'
+            timeout = setTimeout(() => {
+              this.state = 'now'
+              this.message = '지금 클릭'
+              startTime = new Date()
+            }, Math.floor(Math.random() * 1000) + 2000)
           } else if (this.state === 'ready') {
+            clearTimeout(timeout)
             this.state = 'now'
+            this.message = '너무성급 하네요 초록색일떄 클릭'
           } else if (this.state === 'now') {
+            endTime = new Date()
             this.state = 'waiting'
+            this.message = '클릭해서 시작 하세요'
+            this.result.push(endTime - startTime)
           }
         }
       }
