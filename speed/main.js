ec86f4104a53bd7b649046e0246be8578d99d3b1
gilsu <rlftn510@naver.com>
2019-09-18 19:19:37 +0900

    css loader 와 wepack-dev-server

---
+++ speed/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+
+import responseCheck from './responseCheck.vue'
+new Vue(responseCheck).$mount('#root')
---
+++ speed/responseCheck.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div id="screen" v-bind:class="state" @click="onClickScreen">{{message}}</div>
+    <div>
+      <p>평균asd시간 {{state}}</p>
+      <button @submit="onReset">리셋</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      data(){
+        return {
+          result : [],
+          state: 'waiting',
+          message : 'asd'
+        }
+      },
+      methods : {
+        onReset() {
+
+        },
+        onClickScreen (){ 
+          if(this.state === 'waiting') {
+            this.state = 'ready'
+          } else if (this.state === 'ready') {
+            this.state = 'now'
+          } else if (this.state === 'now') {
+            this.state = 'waiting'
+          }
+        }
+      }
+  }
+</script>
+ 
+<style scoped>
+  #screen {
+     width: 300px;
+     height: 200px;
+     text-align: center;
+     user-select: none;
+   }
+  #screen.waiting {
+    background-color: aqua;
+  }
+  #screen.ready {
+    background-color: red;
+    color: white;
+  }
+  #screen.now {
+    background-color: greenyellow;
+  }
+</style>
