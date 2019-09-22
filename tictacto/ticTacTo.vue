7de5027546c6ae4f4dc371d138e8a1d53500e178
gilsu <rlftn510@naver.com>
2019-09-22 18:31:51 +0900

    vue에서 인덱스를 이용한 데이터 변경은 바로 화면에 표출할 수 없다 this.$set([index], index, 변경값) 을 통하여 값을 변경한다.

--- tictacto/ticTacTo.vue
+++ tictacto/ticTacTo.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
+    <div>
+      {{turn}} 님의 턴입니다.
+    </div>
     <table-component v-bind:table-data="tableData"></table-component>
+    <div v-if="winner">{{winner}} 님의 승리!</div>
   </div>
 </template>
 
@@ -17,7 +21,8 @@
           ['','',''],
           ['','',''],
         ],
-        turn : '0'
+        turn : 'O',
+        winner : ''
       }
     },
     methods : {
@@ -27,5 +32,13 @@
 </script>
 
 <style>
-  
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
 </style>
