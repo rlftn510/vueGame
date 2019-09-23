44bcbb85fa268018b37ab2eb11f7288bdb2e7871
gilsu <rlftn510@naver.com>
2019-09-23 20:10:31 +0900

    지뢰찾기 만들기

---
+++ mineGame/main.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import mineSweeper from './mineSweeper.vue'
+new Vue(mineSweeper).$mount('#root')
---
+++ mineGame/mineSweeper.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <mine-form></mine-form>
+    <div>{{timer}}</div>
+    <table-component></table-component>
+    <div>{{result}}</div>
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+  import store from './store'
+  
+  export default {
+    store,
+    components : {
+
+    },
+    computed : {
+      ...mapState(['timer', 'result'])
+    },
+    methods : {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
