7c9479910118a0de310544d9f0cc54262a80faac
gilsu <rlftn510@naver.com>
2019-09-23 19:23:19 +0900

    Vuex 사용 과 slot

---
+++ ticVuex/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+
+import ticTacTo from './ticTacTo.vue'
+new Vue(ticTacTo).$mount('#root')
---
+++ ticVuex/tableComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <table>
+    <slot/>
+  </table>
+</template>
+
+<script>
+  import trComponenet from './trComponent.vue'
+  export default {
+    components : {
+      trComponenet
+    },
+    computed : {
+      tableData() {
+        return this.$store.state.table
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
